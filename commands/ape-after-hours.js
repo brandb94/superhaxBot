57ef6ff0fc9ff7484fee2a911c3657b5765bc6db
brandb94 <brandb94@uw.edu>
2021-06-01 15:59:02 -0700

    switched to using robinhood instead, fixed code for loading html

--- commands/ape-after-hours.js
+++ commands/ape-after-hours.js
@@ -1,12 +1,14 @@
-const $ = require('cheerio');
+const cheerio = require('cheerio');
 const axios = require('axios');
 
 const getAfterHoursPrice = async (url) => {
-    const html = await axios.get(url);
+    const pageResponse = await axios.get(url);
+    const html = pageResponse.data;
+    const $ = cheerio.load(html);
 
    // const html = await page.evaluate(() => document.body.innerHTML);
     // const aferHoursPrice = $('span.symbol-page-header__pricing-price', html).text();
-    const afterHoursPrice = $('div.css-65fur7', html).text();
+    const afterHoursPrice = $('div.css-65fur7').text();
 
     console.log(afterHoursPrice);
 
@@ -22,7 +24,7 @@ module.exports = {
     description: '!hax:ape-after-hours [stock symbol]',
     execute: async (message, args) => {
         const symbol = args[0];
-        const url = `https://www.nasdaq.com/market-activity/stocks/${symbol}/after-hours`;
+        const url = `https://www.robinhood.com/stocks/${symbol}`;
 
         const afterHoursPrice = await getAfterHoursPrice(url);
         message.channel.send(`${symbol.toUpperCase()} After hours price: ${afterHoursPrice}`);
